eceb028e5bf34513f734e2a641734209380305ae
Lukas Meisegeier <lukas.meisegeier@ottogroup.com>
2019-01-22 10:54:51 +0100

    Added message parameter

--- src/main/kotlin/de/klg71/kotlintestplugin/KotlinTestPlugin.kt
+++ src/main/kotlin/de/klg71/kotlintestplugin/KotlinTestPlugin.kt
@@ -5,8 +5,13 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.tasks.TaskAction
 
+open class KotlinTestExtension(
+        var message:String = "Hello World")
+
 open class KotlinTestPlugin : Plugin<Project> {
     override fun apply(project: Project) {
+        project.extensions.create<KotlinTestExtension>("kotlintestextension", KotlinTestExtension::class.java)
+
         project.tasks.create<TestTask>("kotlinTestPlugin", TestTask::class.java).run {
             description = "Print test message"
             group = "TestPlugin"
@@ -17,8 +22,9 @@ open class KotlinTestPlugin : Plugin<Project> {
 open class TestTask : DefaultTask() {
     @TaskAction
     fun testAction() {
-        println("Hello World")
+        project.extensions.findByType(KotlinTestExtension::class.java)!!.run {
+            println(message)
+        }
     }
-
 }
 
